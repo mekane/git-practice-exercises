de5cdc8e88ccedd9b8f2f8adbe50d98c3618f87b
Marty Kane <marty.kane@cmegroup.com>
2020-06-24 16:20:05 -0500

    Add code to update the gran total of the whole cart based on the individual product totals

--- cart.js
+++ cart.js
@@ -2,6 +2,7 @@
 
 function updateCart() {
     updateProductRowTotalCosts();
+    updateCartGrandTotal();
 }
 
 function updateProductRowTotalCosts() {
@@ -20,14 +21,32 @@ function updateTotalCostForProduct(productEl) {
 
     console.log(`compute product ${name}: ${cost} * ${quantity}`);
 
-    const costWithoutDollarSign = cost.slice(1);
-    const costAsNumber = parseFloat(costWithoutDollarSign);
+    const costAsNumber = parseCost(cost);
     const quantityAsNumber = parseInt(quantity);
     const totalCost = quantityAsNumber * costAsNumber;
 
     totalCostEl.textContent = `$${totalCost.toFixed(2)}`;
 }
 
+function updateCartGrandTotal() {
+    let grandTotal = 0;
+
+    const totals = document.querySelectorAll('.cart__product-total');
+    totals.forEach(total => {
+        const productTotal = parseCost(total.textContent);
+        console.log(total.textContent + ' + ' + productTotal);
+        grandTotal += productTotal;
+    });
+
+    const grandTotalEl = document.querySelector('.cart__total-cost');
+    console.log('grand total: ' + grandTotal);
+    grandTotalEl.textContent = `$${grandTotal.toFixed(2)}`;
+}
+
+function parseCost(costAsStringWithDollarSign) {
+    const costWithoutDollarSign = costAsStringWithDollarSign.slice(1);
+    return parseFloat(costWithoutDollarSign);
+}
 
 if (document.readyState !== 'loading') {
     updateCart();
